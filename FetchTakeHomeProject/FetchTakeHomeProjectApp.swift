378438bb028181be97a77aede734fd685c7bac75
Brian Brandenburg <bcbr227@uky.edu>
2025-04-21 21:08:57 -0400

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Casey Brandenburg on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchTakeHomeProjectTests/FetchTakeHomeProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectTests.swift
+//  FetchTakeHomeProjectTests
+//
+//  Created by Casey Brandenburg on 4/21/25.
+//
+
+import Testing
+@testable import FetchTakeHomeProject
+
+struct FetchTakeHomeProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
